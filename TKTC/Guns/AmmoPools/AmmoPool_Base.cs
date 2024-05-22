e74b1ba9009c8c5f4260bddae4794de9f0428cdc
RivinwinCW <rivinwincw@gmail.com>
2024-05-22 08:16:47 -0400

    AmmoPool__Base and Gun_Base integration, Projectile Pooling
    Initial code upload for magezine projectile pooling,
    Gun_Base bolt drop logic,
    Gun_Base began moving SelectorSwitch to seperate class

--- TKTC/Guns/AmmoPools/AmmoPool_Base.cs
+++ TKTC/Guns/AmmoPools/AmmoPool_Base.cs
@@ -1,40 +1,48 @@
 ﻿using System.Collections.Generic;
+using TKTC.Guns.Projectiles;
 using UnityEngine;
 
 namespace TKTC.Guns.AmmoPools
 {
-	internal abstract class AmmoPool_Base
+	internal class AmmoPool_Base
 	{
-		// VARIABLES
-		private System.Type? projectileType; // need to decide if this should be nullable or assign a default projectile_bullet class
+        // VARIABLES
+        protected bool autoRequestRefillFromParentCollections;
+        protected List<AmmoPool_Collection> parentCollections = new();
+
+		private int poolSize;
+        const int MAX_POOL_SIZE = 256;
+        protected byte minimumPoolSize = 2; // We generally want to base the pool size off of clipSize but in cases where clipSize is small and reload times are small, like rocket launchers, this will be necessary to set
+		protected bool usingProjectilePool;
+		protected List<Projectile_Base> projectilePool;
+
+        protected System.Type projectileType = typeof(Projectile_Base); // Id like to use null, but nullable types suck, so check for projectileType == Projectile_Base as a failure state
 		public System.Type ProjectileType
 		{
 			get { return projectileType; }
 			internal set { projectileType = value; }
 		}
 
-		private float amount;
+        protected float amount;
 		public float Amount
 		{
 			get { return amount; }
 			internal set { amount = value; }
 		}
 
-		private float clipSize;
+        protected float clipSize;
 		public float ClipSize
 		{
 			get { return clipSize; }
 			internal set { clipSize = value; }
 		}
 
-		private float capacity;
+        protected float capacity;
 		public float Capacity
 		{
 			get { return capacity; }
 			internal set { capacity = value - (value % clipSize); } // When setting with Property enforce multiple of clipsize
 		}
-
-		private List<AmmoPool_Collection> parentCollections = new();
 
 		// CONSTRUCTORS
 		public AmmoPool_Base()
@@ -45,9 +53,7 @@ namespace TKTC.Guns.AmmoPools
 		public AmmoPool_Base(float initialAmount, float newCapacity, System.Type newProjectileType)
 		{
 			SetValues(initialAmount, newCapacity, newProjectileType);
-#if DEBUG
-			if (capacity == 0f) TKTC.Logger.LogDebug("AmmoPool created with zero capacity!");
-#endif
+			if (capacity < 1f) TKTC.Logger.LogDebug("AmmoPool created with <1.0f capacity!");
 		}
 
 		public AmmoPool_Base(AmmoPool_Base copyFrom)
@@ -55,11 +61,18 @@ namespace TKTC.Guns.AmmoPools
 			SetValues(copyFrom.Amount, copyFrom.Capacity, copyFrom.ProjectileType);
 		}
 
-		// METHODS
-		public bool IsEmpty => amount <= 0f;
+        protected virtual void SetValues(float newAmount, float newCapacity, System.Type newProjectileType)
+        {
+            capacity = Mathf.Min(newCapacity, 0f);
+            amount = Mathf.Clamp(newAmount, 0f, capacity);
+            projectileType = newProjectileType;
+        }
+
+        // METHODS
+        public bool IsEmpty => amount <= 0f;
 		public bool IsFull => amount >= 0f;
 
-		private float AddAmmo(float toAdd)
+        protected float AddAmmo(float toAdd)
 		{
 			float willAdd = Mathf.Min(capacity - amount, toAdd);
 			amount += willAdd;
@@ -67,7 +80,7 @@ namespace TKTC.Guns.AmmoPools
 			return toAdd; // Return remainder
 		}
 
-		public bool AddAmmo(AmmoPool_Base toMerge)
+		internal bool AddAmmo(AmmoPool_Base toMerge)
 		{
 			// Check if the type we are pulling from is of the same type or a base type of this pool, only allow loading into a same or derived ammo pool
 			if (IsFull || !toMerge.GetType().IsAssignableFrom(this.GetType())) return false;
@@ -76,7 +89,7 @@ namespace TKTC.Guns.AmmoPools
 			return true;
 		}
 
-		public bool TryRemoveOne() // Just for single shot reloading
+		public bool TryRemoveOne() // Just for single shot reloading, itty bit more efficient
 		{
 			if (amount < 1f) return false;
 			amount -= 1f;
@@ -97,16 +110,10 @@ namespace TKTC.Guns.AmmoPools
 			return toRemove;
 		}
 
-		internal abstract AmmoPool_Base Clone(); // needs to return a new object of the same type as 'this'
-		/*{
-			return new AmmoPool_Base(this);
-		}*/
-
-		protected virtual void SetValues(float newAmount, float newCapacity, System.Type newProjectileType)
+		internal virtual AmmoPool_Base Clone()
 		{
-			capacity = Mathf.Min(newCapacity, 0f);
-			amount = Mathf.Clamp(newAmount, 0f, capacity);
-			projectileType = newProjectileType;
+			// This needs to be overridden in child classes to return an object of their own type cast to AmmoPool_Base
+			return new AmmoPool_Base(this);
 		}
 
 		internal void AddedToCollection(AmmoPool_Collection newParent)
@@ -118,5 +125,34 @@ namespace TKTC.Guns.AmmoPools
 		{
 			parentCollections.Remove(oldParent);
 		}
-	}
+
+        public void EnableProjectilePool()
+		{
+			usingProjectilePool = true;
+			projectilePool = new();
+
+			poolSize = Mathf.Clamp((int)Mathf.Ceil(clipSize * 1.1f), minimumPoolSize, MAX_POOL_SIZE); // we do clipsize * 1.1 so that a high fire rate high speed reload gun can mag dump and get 10% into their next clip before the first projetile is available again in the pool
+
+			for (int i = 0; i < poolSize)
+			{
+
+			}
+		}
+
+		public void DisableProjectilePool()
+		{
+
+		}
+
+		private IEnumerable SpawnProjectilesInPool(GameObject toClone, int numToSpawn) // Spawn one projectile into the pool per frame, instead of say 70 at once for one gun when youre adding multiple guns at once.
+		{
+			int numSpawned = 0;
+			while (numSpawned < numToSpawn)
+			{
+				GameObject newProjectile = GameObject.Instantiate(toClone);
+				projectilePool.Add(newProjectile.GetComponent<Projectile_Base>());
+				yield return new WaitForSeconds(0.00001f);
+			}
+		}
+    }
 }
--- TKTC/Guns/Gun_Base.cs
+++ TKTC/Guns/Gun_Base.cs
@@ -2,19 +2,121 @@
 using System.Collections.Generic;
 using System.Text;
 using TKTC.Guns.AmmoPools;
+using TKTC.Guns.Projectiles;
+using UnityEngine;
 
 // Were gonna be cheaky and as a design choice start guns unchambered, unloaded, with the safety on.
 
 namespace TKTC.Guns
 {
-    internal class Gun_Base
-    {
-        // VARIABLES
-        private string _name;
-        private bool roundChambered;
-        private AmmoPool_Base magezine;
-        private AmmoPool_Base reloadSource;
+	public class Gun_Base : MonoBehaviour
+	{
+		public struct SelectorSwitch
+		{
+			public enum SwitchState
+			{
+				Safe,
+				Single,
+				Burst,
+				Auto
+			}
+			public SwitchState state;
+		}
 
-        public override string ToString() { return _name; }
+		// VARIABLES
+		private string _name;
+
+		private bool roundChambered;
+		public bool RoundChambered
+		{
+			get { return roundChambered; }
+			private set { }
+		}
+
+		private Projectile_Base? chamberedProjectile;
+		private SelectorSwitch selectorSwitch;
+		private bool waitOnTriggerToCycleChamber;
+
+		private float singleRoF, burstRoF, automaticRoF;
+		private float burstRoundsCount; // its weird its a float, but energy weapons
+
+		private bool roundChamberedAfterLastCycle;
+		private bool hasBolt = true;
+		private bool canDropBoltUnchambered;
+        private AmmoPool_Base? magezine;
+
+		// EVENTS
+		void Start()
+		{
+			magezine = new AmmoPool_Base();
+		}
+
+		void OnDestroy()
+		{
+			magezine?.DisableProjectilePool();
+        }
+
+		// METHODS
+		public override string ToString() { return _name; }
+
+		public virtual void PullTrigger(bool sustained = false)
+		{
+			if (!roundChambered)
+			{
+				if (canDropBoltUnchambered)
+				{
+					canDropBoltUnchambered = false;
+                    // TODO: sound effect of bolt dropping
+                }
+				return;
+			}
+
+			bool didFire;
+			switch (selectorSwitch.state)
+			{
+				case SelectorSwitch.SwitchState.Single:
+					didFire = Fire();
+					break;
+				case SelectorSwitch.SwitchState.Burst:
+                    StartCoroutine(FireBurst((int)magezine.TryRemoveUpTo(burstRoundsCount)));
+                    break;
+                case SelectorSwitch.SwitchState.Auto:
+					break;
+                default:
+					break;
+			}
+			// Cycle chamber immediately if atuomatic blowback bolt
+			if (!waitOnTriggerToCycleChamber) CycleChamber();
+		}
+
+		protected IEnumerable FireBurst(int numToFire)
+		{
+            yield return new WaitForSeconds(burstRoF);
+        }
+
+		public virtual void ReleaseTrigger(bool firedOnPull)
+		{
+			// Cycle chamber on mouse release if manual bolt action
+            if (waitOnTriggerToCycleChamber) CycleChamber();
+		}
+
+		protected virtual bool Fire()
+		{
+			if (roundChambered)
+			{
+				chamberedProjectile.Fire();
+			}
+			return false;
+		}
+
+		protected bool CycleChamber()
+		{
+            // if (roundChamberedAfterLastCycle) TODO: trigger ejection animation
+            roundChambered = magezine.TryRemoveOne();
+            roundChamberedAfterLastCycle = roundChambered;
+			if (hasBolt) canDropBoltUnchambered = true;
+
+            return roundChambered;
+		}
 	}
 }
